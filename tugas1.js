dcd4235d8112abf21ab73ebff27eef3fa695babb
wildan <wildan.syarif1021@gmail.com>
2019-11-07 14:52:39 +0700

    tugas niomic
    tugas niomic

---
+++ tugas1.js
@@ -0,0 +1,5 @@
+var namaDepan = 'Budi';
+let namaBelakang = 'Santoso';
+const umur = 24;
+
+console.log('Nama teman saya adalah '+namaDepan+' dan nama belakang teman saya adalah '+namaBelakang+' dan umurnya adalah '+umur);
